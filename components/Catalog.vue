a465ae84dba20e033c26eb18a8e31f0aa73a0614
Cade Li <cadecode@foxmail.com>
2020-07-29 13:47:29 +0800

    [Fix]修复了目录定位标题的位置误差

--- components/Catalog.vue
+++ components/Catalog.vue
@@ -46,32 +46,32 @@
                         this.show = false
                     }
                 })
-            })
-            this.$nextTick(() => {
-                // catalog follow scroll
-                const getPositionInPage = this.$tool.getPositionInPage
-                const hElements = document.querySelectorAll('.markdown-content h2,.markdown-content h3')
-                ;[].forEach.call(hElements, (item) => {
-                    this.heights.push(getPositionInPage(item).top - 70)
-                })
-                const list = document.querySelectorAll('.catalog-content li')
-                const arr = this.heights
-                const len = arr.length
-                window.addEventListener('scroll', () => {
-                    const scrollTop = this.$tool.getScrollOffset().top
-                    for (let i = 0; i < len - 1; i++) {
-                        if (scrollTop >= arr[i] && scrollTop < arr[i + 1]) {
-                            this.$tool.addClass(list[i], 'catalog-item-active')
-                        } else {
-                            this.$tool.removeClass(list[i], 'catalog-item-active')
+                setTimeout(() => {
+                    // catalog follow scroll
+                    const getPositionInPage = this.$tool.getPositionInPage
+                    const hElements = document.querySelectorAll('.markdown-content h2,.markdown-content h3')
+                    ;[].forEach.call(hElements, (item) => {
+                        this.heights.push(getPositionInPage(item).top - 70)
+                    })
+                    const list = document.querySelectorAll('.catalog-content li')
+                    const arr = this.heights
+                    const len = arr.length
+                    window.addEventListener('scroll', () => {
+                        const scrollTop = this.$tool.getScrollOffset().top
+                        for (let i = 0; i < len - 1; i++) {
+                            if (scrollTop >= arr[i] && scrollTop < arr[i + 1]) {
+                                this.$tool.addClass(list[i], 'catalog-item-active')
+                            } else {
+                                this.$tool.removeClass(list[i], 'catalog-item-active')
+                            }
+                            if (scrollTop >= arr[len - 1]) {
+                                this.$tool.addClass(list[len - 1], 'catalog-active')
+                            } else {
+                                this.$tool.removeClass(list[len - 1], 'catalog-active')
+                            }
                         }
-                        if (scrollTop >= arr[len - 1]) {
-                            this.$tool.addClass(list[len - 1], 'catalog-active')
-                        } else {
-                            this.$tool.removeClass(list[len - 1], 'catalog-active')
-                        }
-                    }
-                })
+                    })
+                }, 1000)
             })
         }
     }
