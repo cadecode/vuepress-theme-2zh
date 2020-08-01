d2ed236839cb843c993ded90c2e4705c368e3a02
Cade Li <cadecode@foxmail.com>
2020-08-01 11:57:45 +0800

    [Chore]优化组件和页面 1. 列表卡片组件化 2. 时间显示效果去 0

--- layouts/Tag.vue
+++ layouts/Tag.vue
@@ -1,45 +1,24 @@
 <template>
     <div class="tag-container">
-                <p class="tag-name">
-                    <i class="el-icon-collection" />
-                    {{tagName}}
-                </p>
-                <el-row>
-                    <el-col :xs="24" :sm="12" :md="8" v-for="(item, index) in pages" :index="index">
-                        <div class="index-card-wrapper">
-                            <el-card class="index-card" shadow="always" :body-style="{ padding: '0px' }">
-                                <div class="index-card-cover" :style="`background-image: url(${item.cover});`"></div>
-                                <div class="index-card-desc">{{item.desc}}</div>
-                                <div class="index-card-pined" v-if="item.pined">
-                                    <i class="el-icon-s-flag" />
-                                </div>
-                                <router-link :to="item.path">
-                                    <div class="index-card-title">
-                                        {{item.title}}
-                                    </div>
-                                <div class="index-card-meta">
-                                    <div class="index-card-date">
-                                        <i class="el-icon-edit-outline" />
-                                        {{item.date}}
-                                    </div>
-                                    <div class="index-card-tag">
-                                        <i class="el-icon-paperclip" />
-                                        {{item.tag}}
-                                    </div>
-                                </div>
-                                </router-link>
-                            </el-card>
-                        </div>
-                    </el-col>
-                </el-row>
+        <p class="tag-name">
+            <i class="el-icon-collection" />
+            {{tagName}}
+        </p>
+        <Cards :pages="pages" />
     </div>
 </template>
 
 <script>
     import AllMixin from '../components/AllMixin'
+
+    import Cards from '../components/Cards'
+
     export default {
         name: "Tag",
-        mixins:[AllMixin],
+        mixins: [AllMixin],
+        components:{
+            Cards
+        },
         data() {
             return {
                 pages: [],
@@ -56,7 +35,7 @@
                 this.pages.push({
                     cover: item.frontmatter.cover || `/cover/${matchedLength - matchedPages.indexOf(item)}.png`,
                     title: item.frontmatter.title || 'No title',
-                    date: day(item.frontmatter.date || '1970/1/1').format('YYYY-MM-DD'),
+                    date: day(new Date(item.frontmatter.date || '1970/1/1')).format('YYYY-M-D'),
                     desc: item.frontmatter.description || 'No description',
                     tag: Array.isArray(item.frontmatter.tag) ? item.frontmatter.tag[0] : item.frontmatter.tag || 'No tag',
                     pined: !!item.frontmatter.pined,
