6fd7f1be55eb07030768b56e2e2e42db816d887f
Cade Li <cade.li@qq.com>
2020-08-20 22:58:25 +0800

    [Chore]调整评论区样式

--- layouts/Info.vue
+++ layouts/Info.vue
@@ -2,15 +2,20 @@
     <div class="info-container">
         <div class="post-title">{{title}}</div>
         <Content class="markdown-content" />
+        <Comment />
     </div>
 </template>
 
 <script>
     import AllMixin from '../components/AllMixin'
+    import Comment from '../components/Comment'
 
     export default {
         name: "Info",
-        mixins:[AllMixin],
+        mixins: [AllMixin],
+        components: {
+            Comment
+        },
         data() {
             return {
                 title: ''
